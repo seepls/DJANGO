68140031a07e02eaf4a65631d195a99cb41ebba8
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2019-03-16 16:10:35 +0530

    Create methods.py

---
+++ DJANGO-REST-FRAMEWORK/methods.py
@@ -0,0 +1,39 @@
+"""
+method called before dispatching to the handler method 
+"""
+.check_permissions(self, request)
+.check_throttles(self, request)
+.perform_content_negotiation(self, request, force= False)
+
+"""Dispatch methods
+directly called by View's dispatch method
+any action before or after calling handler methods
+"""
+.get()
+.post()
+.put()
+.patch()
+.delete()
+
+
+-----------------------------------
+
+.initial(self,request,*args,**kwargs)
+ 
+ """ perform any actions need to occur before the handler method gets called . """
+ 
+ .handle_exception(self, exc)
+ 
+ """
+ any exception thrown by handler method passed to this method . enforce permission and throttling and perform content negotiation
+ 
+ """
+ 
+ .handle_exception(self , exc)
+ 
+ """ returns response instance or raises exception"""
+ 
+ .initialize_request(self, request,*args, **kwargs)
+ 
+ 
+ 
